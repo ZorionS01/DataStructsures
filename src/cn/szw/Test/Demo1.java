17a8befa8432be9c8f16d68769495a1f2b946e19
xiaosong <2370760064@qq.com>
2023-03-29 18:13:43 +0800

    dataStruct hot-fix 1

--- src/cn/szw/Test/Demo1.java
+++ src/cn/szw/Test/Demo1.java
@@ -16,6 +16,7 @@ public class Demo1 {
         System.out.println(Math.pow(16, 15)%4731);
         System.out.println("111111111111111");
         System.out.println("2222222222222222");
+        System.out.println("hot-fix");
     }
 
     public static void isSuShu(int n){
